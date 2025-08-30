76a3be2c9c772cf8686fe5b76e3ee40dd2729480
fnkymnky <cssawesome@gmail.com>
2025-08-30 20:44:10 +0100

    Working header template part

--- themes/ceiba-renewables-2025/functions.php
+++ themes/ceiba-renewables-2025/functions.php
@@ -12,20 +12,20 @@ add_action('enqueue_block_editor_assets', function () {
   wp_enqueue_style('ceiba-tokens-editor', get_stylesheet_directory_uri() . '/assets/tokens.css', [], '1.0');
 });
 
-// /* Curate allowed blocks */
-// add_filter( 'allowed_block_types_all', function( $allowed, $ctx ) {
-//   return [
-//     'core/paragraph','core/heading','core/list', 'core/list-item', 'core/quote',
-//     'core/separator','core/spacer','core/image','core/gallery',
-//     'core/cover','core/media-text','core/buttons','core/button',
-//     'core/columns','core/column','core/group','core/row','core/stack', 'core/table', 'core/accordion', 'core/details',
-//     'core/cover',
-//     'ceiba/content-card', 'ceiba/image-frame', 'ceiba/map-embed', 'ceiba/testimonials-carousel', 'ceiba/callout',
-//     'ceiba/testimonial', 'ceiba/case-study', 'ceiba/case-studies-carousel', 'ceiba/content-section', 'ceiba/content-columns', 'ceiba/team-grid', 'ceiba/content-grid',
-//     'ceiba/content-grid-item'
+/* Curate allowed blocks */
+add_filter( 'allowed_block_types_all', function( $allowed, $ctx ) {
+  return [
+    'core/paragraph','core/heading','core/list', 'core/list-item', 'core/quote',
+    'core/separator','core/spacer','core/image','core/gallery',
+    'core/cover','core/media-text','core/buttons','core/button',
+    'core/columns','core/column','core/group','core/row','core/stack', 'core/table', 'core/accordion', 'core/details',
+    'core/cover',
+    'ceiba/content-card', 'ceiba/image-frame', 'ceiba/map-embed', 'ceiba/testimonials-carousel', 'ceiba/callout',
+    'ceiba/testimonial', 'ceiba/case-study', 'ceiba/case-studies-carousel', 'ceiba/content-section', 'ceiba/content-columns', 'ceiba/team-grid', 'ceiba/content-grid',
+    'ceiba/content-grid-item'
 
-//   ];
-// }, 10, 2 );
+  ];
+}, 10, 2 );
 
 
 
