c33a461ec44a19bd37730ad3a90d38bdf81ca02a
fnkymnky <cssawesome@gmail.com>
2025-09-02 21:43:52 +0100

    Re-run build locally to fix hero section override issues.
    Codex kept changing the build files, instead of the source files and npm run build was overriding the wording build files.

--- plugins/ceiba-content-blocks/build/content-card/index.asset.php
+++ plugins/ceiba-content-blocks/build/content-card/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '289ce5403b943a6fbb18');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ba232470bfcd4a449a55');
--- plugins/ceiba-content-blocks/build/hero-section/index.asset.php
+++ plugins/ceiba-content-blocks/build/hero-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '85338ed8deaba8df7c9b');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '85a194221a0f16600956');
