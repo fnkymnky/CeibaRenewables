0471e0c4f1bd59109943e2cd2237469b42f9dfe2
fnkymnky <cssawesome@gmail.com>
2025-09-12 15:01:03 +0100

    Minor tweaks to block names

--- plugins/ceiba-content-blocks/blocks/content-columns/index.js
+++
@@ -1,6 +0,0 @@
-import { registerBlockType } from '@wordpress/blocks';
-import Edit from './edit';
-import save from './save';
-import './style.scss';
-
-registerBlockType('ceiba/content-columns', { edit: Edit, save });
